56a27d313fa03db6a157197d062edc75847ad019
Jorge cid cano <jorgecid1999@gmail.com>
2024-04-30 12:48:38 +0200

    ya

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require("path");
 const mysql = require("mysql");
 const url = require("url");
 const multer = require("multer");
+const { parse } = require('url');
+
 
 const storage = multer.diskStorage({
   destination: "./img",
@@ -16,21 +18,21 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "NODE_PRUEBAS",
+// });
+
+ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "NODE_PRUEBAS",
-});
-
-//  const connection = mysql.createConnection({
-//   host: "82.223.123.233",
-//   port: 3306,
-//   user: "user_pruebanode",
-//   password: "O@3zk8s95",
-//   database: "bd_pruebanode",
-//  });
+  user: "user_pruebanode",
+  password: "O@3zk8s95",
+  database: "bd_pruebanode",
+ });
 
 
 connection.connect((err) => {
@@ -79,7 +81,42 @@ const server = http.createServer((req, res) => {
         }
       );
     });
-  } else if (pathname === "/datos_empleados" && req.method === "GET") {
+  } else if (pathname === "/eliminar-departamento" && req.method === "DELETE") {
+    console.log("Solicitud para eliminar departamento recibida");
+
+    let requestBody = "";
+    req.on("data", (chunk) => {
+      requestBody += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const { idDpt } = JSON.parse(requestBody);
+
+      if (!idDpt) {
+        res.writeHead(400);
+        res.end("El ID del departamento es necesario para eliminarlo");
+        return;
+      }
+
+      connection.query(
+        "DELETE FROM departamento WHERE ID = ?",
+        [idDpt],
+        (error, results) => {
+          if (error) {
+            console.error("Error al eliminar el departamento:", error);
+            res.writeHead(500);
+            res.end("Error al eliminar el departamento de la base de datos");
+            return;
+          }
+
+          console.log("Departamento eliminado correctamente");
+          res.writeHead(200);
+          res.end("Departamento eliminado correctamente");
+        }
+      );
+    });
+  }
+ else if (pathname === "/datos_empleados" && req.method === "GET") {
     console.log("Solicitud de datos de empleados recibida");
 
     connection.query(
@@ -100,9 +137,9 @@ const server = http.createServer((req, res) => {
     );
   } else if (pathname === "/datos_departamentos" && req.method === "GET") {
     console.log("Solicitud de datos de departamentos recibida");
-
+  
     connection.query(
-      "SELECT ID, NOMBRE, DESCRIPCION FROM departamentos",
+      "SELECT ID, NOMBRE FROM departamento",
       (error, results) => {
         if (error) {
           console.error("Error al consultar la base de datos:", error);
@@ -110,13 +147,40 @@ const server = http.createServer((req, res) => {
           res.end("Error al consultar la base de datos");
           return;
         }
-
+  
         const responseData = JSON.stringify(results);
         console.log("Datos de departamentos enviados:", responseData);
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(responseData);
       }
     );
+  } else if (pathname === "/datos_departamento" && req.method === "GET") {
+    const queryObject = url.parse(req.url, true).query;
+    const idDepartamento = queryObject.id;
+  
+    if (idDepartamento) {
+      connection.query(
+        "SELECT ID, NOMBRE,DESCRIPCION FROM departamento WHERE ID = ?",
+        [idDepartamento],
+        (error, results) => {
+          if (error) {
+            console.error("Error al consultar la base de datos:", error);
+            res.writeHead(500);
+            res.end("Error al consultar la base de datos");
+            return;
+          }
+  
+          const responseData = JSON.stringify(results[0]);
+          console.log("Datos del departamento enviados:", responseData);
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(responseData);
+        }
+      );
+    } else {
+      console.error("ID de departamento no proporcionado");
+      res.writeHead(400);
+      res.end("ID de departamento no proporcionado");
+    }
   } else if (pathname === "/datos_empleado" && req.method === "GET") {
     const queryObject = url.parse(req.url, true).query;
     const idEmpleado = queryObject.id;
@@ -179,6 +243,41 @@ const server = http.createServer((req, res) => {
       );
     });
   } 
+  else if (pathname === "/guardar-departamento") {
+    console.log("Solicitud para guardar departamento recibida");
+  
+    // Analizar el cuerpo de la solicitud para obtener los datos del departamento
+    upload.none()(req, res, (err) => {
+      if (err) {
+        console.error("Error al analizar el cuerpo de la solicitud:", err);
+        res.writeHead(500);
+        res.end("Error al procesar la solicitud");
+        return;
+      }
+  
+      const { nombre, descripcion } = req.body;
+  
+      // Insertar el departamento en la base de datos
+      connection.query(
+        'INSERT INTO departamento (NOMBRE, DESCRIPCION) VALUES (?, ?)',
+        [nombre, descripcion],
+        (error, results) => {
+          if (error) {
+            console.error("Error al insertar departamento en la base de datos:", error);
+            res.writeHead(500);
+            res.end("Error al guardar departamento en la base de datos");
+            return;
+          }
+  
+          console.log("Departamento guardado correctamente en la base de datos");
+          res.writeHead(200);
+          res.end("Departamento guardado correctamente en la base de datos");
+        }
+      );
+    });
+  }
+  
+
   else if (pathname === "/actualizar-empleado" && req.method === "POST") {
     console.log("Solicitud para actualizar empleado recibida");
     upload.single("imagen")(req, res, (err) => {
@@ -235,7 +334,49 @@ const server = http.createServer((req, res) => {
         res.end("Error al procesar la solicitud");
       }
     });
-  } else {
+  } 
+  else if (pathname === "/actualizar-departamento" && req.method === "POST") {
+    console.log("Solicitud para actualizar departamento recibida");
+
+    upload.none()(req, res, (err) => { // Usamos upload.none() para indicar que no esperamos archivos
+        if (err) {
+            console.error("Error al cargar los datos del formulario:", err);
+            res.writeHead(500);
+            res.end("Error al cargar los datos del formulario");
+            return;
+        }
+
+        try {
+            const { idDpt, nombre, descripcion } = req.body;
+
+            console.log("Datos recibidos del cliente:", { idDpt, nombre, descripcion });
+
+            // Ejecutar la consulta para actualizar el departamento en la base de datos
+            connection.query(
+                'UPDATE departamento SET NOMBRE = ?, DESCRIPCION = ? WHERE ID = ?',
+                [nombre, descripcion, idDpt],
+                (error, results) => {
+                    if (error) {
+                        console.error("Error al actualizar departamento en la base de datos:", error);
+                        res.writeHead(500);
+                        res.end("Error al actualizar departamento en la base de datos");
+                        return;
+                    }
+
+                    console.log("Departamento actualizado correctamente en la base de datos");
+                    res.writeHead(200);
+                    res.end("Departamento actualizado correctamente en la base de datos");
+                }
+            );
+        } catch (error) {
+            console.error("Error al procesar la solicitud:", error);
+            res.writeHead(400);
+            res.end("Error al procesar la solicitud");
+        }
+    });
+}
+
+else {
     let filePath = "." + pathname;
     if (filePath === "./") {
       filePath = "./index.html";
