53565954ab75568126e7fff79a719622ae51db44
Jorge cid cano <jorgecid1999@gmail.com>
2024-05-06 16:35:35 +0200

    ya

--- index.js
+++ index.js
@@ -26,14 +26,22 @@ const upload = multer({ storage: storage });
 //   database: "NODE_PRUEBAS",
 // });
 
+//  const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "user_pruebanode",
+//   password: "O@3zk8s95",
+//   database: "bd_pruebanode",
+//  });
+
+
  const connection = mysql.createConnection({
-  host: "localhost",
+  host: "82.223.123.233",
   port: 3306,
   user: "user_pruebanode",
   password: "O@3zk8s95",
   database: "bd_pruebanode",
  });
-
 
 connection.connect((err) => {
   if (err) {
